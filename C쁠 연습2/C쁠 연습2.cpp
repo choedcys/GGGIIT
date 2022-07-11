856dbbdff33a7e4c11443757584041d97f44834c
choedcys <choedcys@naver.com>
2022-07-11 23:33:32 +0900

    코딩테스트 교재-거스름돈

--- C쁠 연습2/C쁠 연습2.cpp
+++ C쁠 연습2/C쁠 연습2.cpp
@@ -1,13 +1,31 @@
-//18-12
 #include<iostream>
-#include"header.h"
 using namespace std;
+//최소 동전 갯수 교환 문제(그리디) 
+
 
 int main() {
-	int a[] = { 1,2,3,4,5 };
-	Calculator* p = new GoodCalc();
-	cout << p->add(2, 3) << endl;
-	cout << p->subtract(2, 3) << endl;
-	cout << p->average(a, 5) << endl;
-	delete p;
+	int money, cnt = 500;
+	int wallet[4];
+	cout << "가격을 입력하시오: ";
+	cin >> money;
+	for (int i = 0; i < 4; i++) {
+		wallet[i] = int(money / cnt);
+		money -= wallet[i]*cnt;
+		if (i % 2 == 0) {
+			cnt /= 5;
+		}
+		else {
+			cnt /= 2;
+		}
+	}//손님 지갑에 돈을 넣어줌
+	cnt = 500;
+	for (int i = 0; i < 4; i++) {
+		cout << cnt<<"원: "<< wallet[i]<<endl;
+		if (i % 2 == 0) {
+			cnt /= 5;
+		}
+		else {
+			cnt /= 2;
+		}
+	}//지갑의 돈을 확인함
 }
--- C쁠 연습2/header.h
+++ C쁠 연습2/header.h
@@ -1,16 +1,1 @@
-//18-12
-class Calculator {
-public:
-	Calculator();
-	virtual int add(int a, int b) = 0; // 두정수의합리턴
-	virtual int subtract(int a, int b) = 0; // 두정수의차리턴
-	virtual double average(int a[], int size) = 0; // 배열a의평균리턴size는배열의크기
-};
-
-class GoodCalc :public Calculator {
-public:
-	GoodCalc();
-	int add(int a, int b);
-	int subtract(int a, int b) ;
-	double average(int a[], int size);
-};
+//18-13
