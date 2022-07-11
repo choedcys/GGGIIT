3d02474fda2cc852c7edb794ac9af3139a7befe2
choedcys <choedcys@naver.com>
2022-07-11 23:45:23 +0900

    코딩테스트 교재-거스름돈, 답지공부

--- C쁠 연습2/C쁠 연습2.cpp
+++ C쁠 연습2/C쁠 연습2.cpp
@@ -1,31 +1,17 @@
 #include<iostream>
 using namespace std;
-//최소 동전 갯수 교환 문제(그리디) 
+//최소 동전 갯수 교환 문제(그리디), 책내용을 이해후 c언어로 구현
 
 
 int main() {
-	int money, cnt = 500;
-	int wallet[4];
+	int money, cnt = 0;
+	int wallet[4] = {500,100,50,10};
 	cout << "가격을 입력하시오: ";
 	cin >> money;
 	for (int i = 0; i < 4; i++) {
-		wallet[i] = int(money / cnt);
-		money -= wallet[i]*cnt;
-		if (i % 2 == 0) {
-			cnt /= 5;
-		}
-		else {
-			cnt /= 2;
-		}
-	}//손님 지갑에 돈을 넣어줌
-	cnt = 500;
-	for (int i = 0; i < 4; i++) {
-		cout << cnt<<"원: "<< wallet[i]<<endl;
-		if (i % 2 == 0) {
-			cnt /= 5;
-		}
-		else {
-			cnt /= 2;
-		}
-	}//지갑의 돈을 확인함
+		cnt += int(money / wallet[i]);
+		//money -= wallet[i]*int(money / wallet[i]); 내가 짠 부분
+		money %= wallet[i]; //훨씬더 간결한 내용
+	}
+	cout << cnt;
 }
