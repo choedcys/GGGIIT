9a4da718a8d0bde51bbd2d492fc5d70435b19354
choedcys <choedcys@naver.com>
2022-07-17 08:18:43 +0900

    1이 될때 까지

--- C쁠 연습2/C쁠 연습2.cpp
+++ C쁠 연습2/C쁠 연습2.cpp
@@ -2,46 +2,23 @@
 using namespace std;
 
 int main() {
-	int x, y,ans=0,min,comp;
-	cout << "행 개수: ";
-	cin >> x;
-	cout << "열 개수: ";
-	cin >> y;
-	int **card;
-	int* list = new int[x];
-	card = (int**) malloc(sizeof(int*)*x);
-	for (int row = 0; row < x; row++) {
-		card[row] = new int[y];
-	}
-	cout << "카드 입력: "<<endl;
-	for (int row = 0; row < x; row++) {
-		for (int col = 0; col < y; col++) {
-			cout << row << "행" << col << "열" << " 입력";
-			cin>>card[row][col];
+	int N, K,num,cnt=0;
+	cout << "피제수 입력: ";
+	cin >> N;
+	cout << "제수 입력: ";
+	cin >> K;
+	while (1) {
+		if (N / K == 0) {
+			cnt+=N%K-1;
+			break;
 		}
-	}
-	for (int row = 0; row < x; row++) {
-		for (int col = 0; col < y; col++) {
-			cout <<card[row][col]<<" ";
+		if (N % K == 0) {
+			N /= K;
 		}
-		cout << endl;
-	}
-	min = card[0][0];
-	int row = 0;
-	for (int row = 0; row < x; row++) {
-		min = card[row][0];
-		for (int col = 0; col < y; col++) {
-			if (min > card[row][col])
-				min = card[row][col];
+		else {
+			N--;
 		}
-		list[row] = min;
+		cnt++;
 	}
-	min = list[0];
-	for (int row = 0; row < x; row++) {
-		if (min < list[row]) {
-			ans = row;
-			min = list[row];
-		}
-	}
-	cout <<"정답: "<< ans << "행";
+	cout << cnt;
 }
