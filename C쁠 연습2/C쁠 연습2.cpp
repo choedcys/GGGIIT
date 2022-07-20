caef054fd4b7edaed27e202f7c5d356cc1aef255
choedcys <choedcys@naver.com>
2022-07-20 18:58:33 +0900

    템플릿-1

--- C쁠 연습2/C쁠 연습2.cpp
+++ C쁠 연습2/C쁠 연습2.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
-#include"header.h"
 using namespace std;
+template<typename Bread>
+Bread bigger(Bread temp1, Bread temp2) {
+	if (temp1 > temp2)
+		return temp1;
+	else if (temp1 == temp2)
+		return 0;
+	else
+		return temp2;
+}
 
 int main() {
-	Ctrl good;
-	char key;
-	while (1) {
-		cout << "계속?";
-		cin >> key;
-		if (key == 'n' || key == 'N')
-			break;
-		if (good.Give() == 0) {
-			cout << good;
-			break;
-		}
-	}
+	int a = 20, b = 50;
+	char c = 'a', d = 'z';
+	cout << "bigger(20, 50)의 결과는 " << bigger(a, b) << endl;
+	cout << "bigger('a', 'z')의 결과는 " << bigger(c, d) << endl;
 }
