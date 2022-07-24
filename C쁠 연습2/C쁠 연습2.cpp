efb52e0353cd1d500d1f9d559358b81808cf4037
choedcys <choedcys@naver.com>
2022-07-24 11:12:04 +0900

    템플릿-7

--- C쁠 연습2/C쁠 연습2.cpp
+++ C쁠 연습2/C쁠 연습2.cpp
@@ -2,19 +2,31 @@
 #include"header.h"
 using namespace std;
 
-template<typename T>
-T biggest(T* temp1, int temp2) {
-	T res = temp1[0];
-	for (int i = 0; i < temp2; i++) {
-		if (res < temp1[i])
-			res = temp1[i];
+template<typename T, size_t N, typename U,size_t M>
+bool equalArrays(T (&temp1)[N], U (&temp2)[M]) {
+	if (N == M) {
+		for (int i = 0; i < N; i++) {
+			if (temp1[i] != temp2[i])
+				return false;
+		}
+		return true;
 	}
-	return res;
+	else {
+		return false;
+	}
 }
 
 int main() {
-	int x[] = { 1, 10, 100, 5, 4 };
-	cout << biggest(x,5) << endl; // 5는배열x의크기
-	double y[] = { 6.4, 3.3, -200.6, 7.7, 8.8, 20.3 };
-	cout << biggest(y,6); // 6는배열y의크기
+	int x1[] = { 1, 10, 100, 5, 4 };
+	int x2[] = { 1, 10, 100, 5, 4 };
+	if (equalArrays(x1, x2))
+		cout << "두개의 배열은 같습니다." << endl;
+	else
+		cout << "두개의 배열은 다릅니다." << endl;
+	double y1[] = { 6.4, 3.3, -200.6, 7.7, 8.8, 20.3 };
+	double y2[] = { 6.4, 3.4, -200.6, 7.7, 8.8, 20.3 };
+	if (equalArrays(y1, y2))
+		cout << "두개의 배열은 같습니다." << endl;
+	else
+		cout << "두개의 배열은 다릅니다." << endl;
 }
