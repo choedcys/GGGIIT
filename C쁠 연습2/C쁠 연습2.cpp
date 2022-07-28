9bec58e1f4a0c87dbb4bf763c900fc9646599083
choedcys <choedcys@naver.com>
2022-07-28 18:19:13 +0900

    템플릿-13

--- C쁠 연습2/C쁠 연습2.cpp
+++ C쁠 연습2/C쁠 연습2.cpp
@@ -2,45 +2,48 @@
 using namespace std;
 
 template<typename T>
-class Node {
+class SET {
+	T arr[20];
+	int cnt = 0;
 public:
-	T data;
-	Node<T>* next;
-	Node() {}
+	SET(){}
+	void add(T temp) {
+		int i;
+		for (i = 0; i < 20; i++) {
+			if (arr[i] == temp)
+				break;
+		}
+		if(i == 20)
+			arr[cnt++] = temp;
+	}
+	void print() {
+		for (int i = 0; i < cnt; i++) {
+			cout << arr[i]<<" ";
+		}
+		cout << endl;
+	}
 };
 
 
-
-
 int main() {
-	cout << "int:";
-	int num;
-	Node<int>* sp = NULL;
-	Node<int>* ep = NULL;
-	Node<int>* cp = NULL;
+	SET<int> a;
+	int key;
 	while (1) {
-		cin >> num;
-		if (num == -1) {
+		cout << "1. ADD\n2. PRINT\n3. EXIT\n==>";
+		cin >> key;
+		if (key == 1) {
+			cout << "INPUT DATA: ";
+			cin >> key;
+			a.add(key);
+		}
+		else if (key == 2) {
+			a.print();
+		}
+		else if (key == 3) {
 			break;
 		}
-		if (sp == NULL) {
-			sp = new Node<int>;
-			ep = sp;
+		else {
+			cout << "잘못된 입력" << endl;
 		}
-		else
-		{
-			ep->next = new Node<int>;
-			ep = ep->next;
-		}
-		ep->data = num;
-		ep->next = NULL;
-	}
-	cp = sp;
-	while (1) {
-		if (cp == NULL) {
-			break;
-		}
-		cout << cp->data << endl;
-		cp = cp->next;
 	}
 }
