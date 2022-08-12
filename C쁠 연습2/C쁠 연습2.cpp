d23a426e4789e7698ca290019e6c703a9650d974
choedcys <choedcys@naver.com>
2022-08-12 19:46:41 +0900

    연산자오버로딩(3)-3 달력 완성

--- C쁠 연습2/C쁠 연습2.cpp
+++ C쁠 연습2/C쁠 연습2.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-	Date d(16,2,29);
+	Date d(20,3,1);
 	cout << "++ 연산자 : ";
-	cout << d++<<endl;
-	cout << "-- 연산자 : ";
+	cout << d++ << endl;
+	cout << "-- 연산자 : ";	
 	cout << d--<<endl;
 	cout<<d;
 	return 0;
--- C쁠 연습2/sub.cpp
+++ C쁠 연습2/sub.cpp
@@ -51,28 +51,26 @@ void Date::dearrange() {
 	}
 	while (1) {
 		if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
-			if (tot < 32)
+			if (tot < 31)
 				break;
 			tot -= 31;
 		}
-		else {
-			if (m == 2) {
-				if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
-					if (tot < 30)
-						break;
-					tot -= 29;
-				}
-				else {
-					if (tot < 29)
-						break;
-					tot -= 28;
-				}
+		else if (m == 2) {
+			if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+				if (tot < 29)
+					break;
+				tot -= 29;
 			}
 			else {
-				if (tot < 31)
+				if (tot < 28)
 					break;
-				tot -= 30;
+				tot -= 28;
 			}
+		}
+		else {
+			if (tot < 30)
+				break;
+			tot -= 30;
 		}
 		m++;
 	}
